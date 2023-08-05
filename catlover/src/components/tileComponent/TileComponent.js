b2f0db02799072a09b4332e9669b9ecac36077ce
Vaggelis Dimitropoulos <vag.dimitrop@gmail.com>
2023-08-05 15:19:04 +0300

    Moving initial get call in HomePage.js along with needed changes in TileComponents.js | Reformatted code in HomePage.js & TileComponent.js

--- catlover/src/components/tileComponent/TileComponent.js
+++ catlover/src/components/tileComponent/TileComponent.js
@@ -3,33 +3,17 @@ import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.css';
 import ModalComponent from "../modalComponent/ModalComponent";
 
-const TileComponent = () => {
-    const [catData, setCatImages] = useState([]);
+const TileComponent = (props) => {
+    const {catData} = props;
     const [modalData, setModalData] = useState([]);
     const [modalIsOpen, setModalIsOpen] = useState(false);
-
-
-    const APIKey = 'live_Lel5oW8x7PrQ6TPSOIC2XyoQB9SSfzd4uHE4ukbENfzdOxbO3f1ojNv13BAKUHyj'
-
-    useEffect(() => {
-        const fetchCatImages = async () => {
-            try {
-                const response = await axios.get('https://api.thecatapi.com/v1/images/search?limit=10',
-                    { headers: { 'x-api-key': APIKey } });
-                setCatImages(response.data);
-            } catch (error) {
-                console.error('Error fetching cat images:', error);
-            }
-        };
-        fetchCatImages();
-    }, []);
 
     const onTileClick = (data) => {
         if (data.breeds.length) {
             let selectedEntryData = {
                 showModal: true,
-                url : data.url,
-                breed : data.breeds[0].name,
+                url: data.url,
+                breed: data.breeds[0].name,
                 description: data.breeds[0].description,
                 imageId: data.id
             }
@@ -44,39 +28,25 @@ const TileComponent = () => {
         setModalIsOpen(false);
     };
 
-    const onLoadMoreClick = async () => {
-        try {
-            const response = await axios.get('https://api.thecatapi.com/v1/images/search?limit=10',
-                { headers: { 'x-api-key': APIKey } });
-            setCatImages([...catData, ...response.data]);
-        } catch (error) {
-            console.error('Error fetching cat images:', error);
-        }
-    };
-
     return (
         <div>
-        <div className="gallery-container">
-            <h1>Cat Gallery</h1>
-            <div className="container">
-                <div className="row">
-                    {catData.map((data) => (
-                        <div className="tile col-md-3"
-                             onClick={() => onTileClick(data)}
-                             key={data.id}>
+            <div className="gallery-container">
+                <h1>Cat Gallery</h1>
+                <div className="container">
+                    <div className="row">
+                        {catData.map((data) => (
+                            <div className="tile col-md-3"
+                                 onClick={() => onTileClick(data)}
+                                 key={data.id}>
 
-                            <div className="tile-container">
-                                <img className="tile-image" key={data.id} src={data.url} alt="Cat"/>
+                                <div className="tile-container">
+                                    <img className="tile-image" key={data.id} src={data.url} alt="Cat"/>
+                                </div>
                             </div>
-                        </div>
-                    ))}
+                        ))}
+                    </div>
                 </div>
             </div>
-
-            <button className="load-button"
-                    onClick={() => onLoadMoreClick()}>Load more furry friends!
-            </button>
-        </div>
             <ModalComponent
                 data={modalData}
                 isModalOpen={modalIsOpen}
--- catlover/src/pages/HomePage.js
+++ catlover/src/pages/HomePage.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import TileComponent from "../components/tileComponent/TileComponent";
+import axios from "axios";
 
 const HomePage = () => {
+    const [catData, setCatImages] = useState([]);
+    const APIKey = 'live_Lel5oW8x7PrQ6TPSOIC2XyoQB9SSfzd4uHE4ukbENfzdOxbO3f1ojNv13BAKUHyj'
+
+    useEffect(() => {
+        const fetchCatImages = async () => {
+            try {
+                const response = await axios.get('https://api.thecatapi.com/v1/images/search?limit=10',
+                    {headers: {'x-api-key': APIKey}});
+                setCatImages(response.data);
+            } catch (error) {
+                console.error('Error fetching cat images:', error);
+            }
+        };
+        fetchCatImages();
+    }, []);
+
+    const onLoadMoreClick = async () => {
+        try {
+            const response = await axios.get('https://api.thecatapi.com/v1/images/search?limit=10',
+                {headers: {'x-api-key': APIKey}});
+            setCatImages([...catData, ...response.data]);
+        } catch (error) {
+            console.error('Error fetching cat images:', error);
+        }
+    };
+
     return (
         <div>
-            <TileComponent/>
+            <TileComponent
+                catData={catData}/>
+
+            <button className="load-button"
+                    onClick={() => onLoadMoreClick()}>Load more furry friends!
+            </button>
         </div>
+
+
     );
 };
 
