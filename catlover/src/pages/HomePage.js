fce0c1344cd1dcb2db1ab154f004899524590d0e
Vaggelis Dimitropoulos <vag.dimitrop@gmail.com>
2023-08-05 11:05:07 +0300

    Renaming Home and Favorites .js files

---
+++ catlover/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TileComponent from "../components/tileComponent/TileComponent";
+
+const HomePage = () => {
+    return (
+        <div>
+            <TileComponent/>
+        </div>
+    );
+};
+
+export default HomePage;
